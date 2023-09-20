194fe7caeb7fa11b7dd4265c04b81398cac3a8e3
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-09-20 13:47:02 +0100

    add: Navigation

--- components/Layouts/Navigation/index.tsx
+++ components/Layouts/Navigation/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import { AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import React, { useState } from "react";
 
-type Props = {}
+const Navigation = () => {
+	const [showMobileNav, setShowMobileNav] = useState(false);
 
-const index = (props: Props) => {
-  return (
-    <div>index</div>
-  )
-}
+	return (
+		<>
+			<nav className='flex items-center h-16 lg:h-[88px] fixed bg-white z-50 top-0 w-full navigation-bg border-b border-[#F0F0F0]'>
+				<div className='container flex items-center justify-between'>
+					<div>
+						<Link href='/' passHref>
+							<div className='w-[87px] h-8 md:h-12 xl:w-[97px]'>
+							</div>
+						</Link>
+					</div>
+					<div className='flex items-center space-x-8'>
+						<ul className='space-x-12 hidden xl:flex'>
+							''
+						</ul>
+						<div className='space-x-4 hidden xl:flex'>
+							<Link href='/' target='_blank'
+									className='text-xs w-[153px] lg:text-sm font-medium py-2.5'>
+									Join us
+							</Link>
+						</div>
+					</div>
+					<button onClick={() => setShowMobileNav(true)} className='xl:hidden'>
+					</button>
+				</div>
+			</nav>
+		</>
+	);
+};
 
-export default index
+export default Navigation;
