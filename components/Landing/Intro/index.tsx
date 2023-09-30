4b66c37207aabf2d931df58dcc8e6e9f87cda54f
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-09-30 14:07:10 +0100

    intro.div

--- components/Landing/Intro/index.tsx
+++ components/Landing/Intro/index.tsx
@@ -4,7 +4,41 @@ type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div>index</div>
+    <>
+    <div className="border-b border-gray-700">
+        <section id="" className="container">
+            <div className="grid xl:grid-cols-2 gap- xl:gap- items-start">
+                <div className="mt-[80px]">
+                    <div className="relative">
+                        <div className="flex justify-end relativ absolute right-[150px] -top-[44px]">
+                            <img src="/Images/creative-1@2x.png" width={53} height={72} alt="creative" className='' />
+                        </div>
+                        <h1 className="text-[70px] font-extrabold">getlinked Tech</h1>
+                    </div>
+                    <div className="flex relative">
+                        <h1 className="text-[70px] font-extrabold leading-none">Hackathon <span className='text text-fuchsia-600'>1.0</span></h1>
+                        <div className="flex absolute -right-5 bottom-3">
+                            <div className="">
+                                <img src="/Images/chain.png" width={80} height={82} alt="chain" className='' />
+                            </div>
+                            <div className="">
+                                <img src="/Images/bomb-img.png" width={78} height={32} alt="bomb" className='' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="w-[80%] mt-[8px]">
+                        <p className="text-xl">Participate in getlinked tech Hackathon 2023 stand a chance to win a Big prize</p>
+                    </div>
+                   
+                </div>
+
+                <div className="flex justify-end relative">
+                        <img src="/Images/image-1@2x.png" alt=""  height={641} width={577} className='absolute animate-pulse -translate-x-9' />
+                </div>
+            </div>
+        </section>
+    </div>
+    </>
   )
 }
 
