41011906951e64d639f06fd135029affe6254e93
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-01 14:55:37 +0100

    //

--- components/Landing/Intro/index.tsx
+++ components/Landing/Intro/index.tsx
@@ -20,8 +20,8 @@ const index = (props: Props) => {
                       <h2 className='text-[32px] font-bold'>Introduction to getlinked</h2>
                       <h2><span className='text-[32px] font-bold text text-fuchsia-600'>tech Hackathon 1.0</span></h2>
                     </div>
-                    <div className="mt-[16px]">
-                      <p className='fon font-montserrat text-[14px]'>Our tech hackathon is a melting pot of visionaries, and its purpose is as
+                    <div className="mt-[16px] mb-[100px]">
+                      <p className='fon font-montserrat text-[14px] xl:w-[535px] xl:h-[165px]'>Our tech hackathon is a melting pot of visionaries, and its purpose is as
                         clear as day: to shape the future. Whether you're a coding genius, a 
                         design maverick, or a concept wizard, you'll have the chance to transform 
                         your ideas into reality. Solving real-world problems, pushing the boundaries
