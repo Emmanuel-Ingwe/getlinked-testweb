7ebc71afa366d4ef0023332dd277b379c118c592
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-01 14:25:52 +0100

    add: intro.h2

--- components/Landing/Intro/index.tsx
+++ components/Landing/Intro/index.tsx
@@ -7,13 +7,19 @@ const index = (props: Props) => {
     <>
     <div className="border-b border-gray-700">
         <section id="" className="container">
-            <div className="grid xl:grid-cols-2 gap- xl:gap- items-start">
+            <div className="grid xl:grid-cols-2 gap-5 xl:gap- items-start">
                 <div className="mt-[63px]">
-                  d
+                  <div className="flex items-center">
+                    <img src="/images/the-big-idea-1@2x.png" alt="big-idea" />
+                  </div>
                 </div>
 
                 <div className="mt-[193px]">
-                  d
+                  <div className="flex items-center justify-center">
+                    <div className="f float-right">
+                      <h2 className=''>Introduction to getlinked <span className='text text-fuchsia-600'>tech Hackathon 1.0</span></h2>
+                    </div>
+                  </div>
                 </div>
             </div>
         </section>
