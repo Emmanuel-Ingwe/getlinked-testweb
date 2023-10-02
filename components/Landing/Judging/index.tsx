326115ffcbe9bead88444baab0c7c6ac6dfb5f16
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-03 00:27:51 +0100

    span*para

--- components/Landing/Judging/index.tsx
+++ components/Landing/Judging/index.tsx
@@ -2,35 +2,35 @@ import React from 'react'
 import Button from '../../global/Button'
 
 
-type para = {
-	span: string;
-	para: string;
-};
+      type para = {
+        span: string;
+        para: string;
+      };
 
-const navigationLinks: para[] = [
-	{
-		span: "Innovation and Creativity:",
-		para: "Evaluate the uniqueness and creativity of the solution. Consider whether it addresses a real-world problem in a novel way or introduces innovative features.",
-	},
-	{
-		span: "Functionality:",
-		para: "Assess how well the solution works. Does it perform its intended functions effectively and without major issues? Judges would consider the completeness and robustness of the solution.",
-	},
-	{
-		span: "Impact and Relevance:",
-		para: "Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.",
-	},
-    {
-		span: "Technical Complexity:",
-		para: "Evaluate the technical sophistication of the solution. Judges would consider the complexity of the code, the use of advanced technologies or algorithms, and the scalability of the solution.",
-	},
-  {
-		span: "Adherence to Hackathon Rules:",
-		para: "Judges will Ensure that the team adhered to the rules and guidelines of the hackathon, including deadlines, use of specific technologies or APIs, and any other competition-specific requirements.",
-	},
-];
-
-const index = (props: para) => {
+   
+const index = () => {
+    const items: para[] = [
+      {
+        span: "Innovation and Creativity:",
+        para: "Evaluate the uniqueness and creativity of the solution. Consider whether it addresses a real-world problem in a novel way or introduces innovative features.",
+      },
+      {
+        span: "Functionality:",
+        para: "Assess how well the solution works. Does it perform its intended functions effectively and without major issues? Judges would consider the completeness and robustness of the solution.",
+      },
+      {
+        span: "Impact and Relevance:",
+        para: "Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.",
+      },
+        {
+        span: "Technical Complexity:",
+        para: "Evaluate the technical sophistication of the solution. Judges would consider the complexity of the code, the use of advanced technologies or algorithms, and the scalability of the solution.",
+      },
+      {
+        span: "Adherence to Hackathon Rules:",
+        para: "Judges will Ensure that the team adhered to the rules and guidelines of the hackathon, including deadlines, use of specific technologies or APIs, and any other competition-specific requirements.",
+      },
+    ];
   return (
     <>
     <div className="border-b border-gray-700">
@@ -49,13 +49,11 @@ const index = (props: para) => {
                       <h2><span className='text-[32px] font-bold text text-fuchsia-600'>Key attributes</span></h2>
                     </div>
                     <div className="mt-[16px] mb-[100px]">
-                      <p className='fon font-montserrat text-[14px] xl:w-[535px] xl:h-[165px]'>Our tech hackathon is a melting pot of visionaries, and its purpose is as
-                        clear as day: to shape the future. Whether you're a coding genius, a 
-                        design maverick, or a concept wizard, you'll have the chance to transform 
-                        your ideas into reality. Solving real-world problems, pushing the boundaries
-                        of technology, and creating solutions that can change the world,
-                        that's what we're all about!
-                      </p>
+                    {items.map((item, index) => {
+						             return (
+                      <p className='font-montserrat text-[14px] xl:w-[535px] xl:h-[165px]'><span>{item.span}</span> {item.para}</p>
+                     )}
+                      )}
                     </div>
                     <div className="">
                       <Button label='Read More' />
@@ -66,7 +64,7 @@ const index = (props: para) => {
         </section>
     </div>
     </>
-  )
-}
+  )}
+
 
 export default index
