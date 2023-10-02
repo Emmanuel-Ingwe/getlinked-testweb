0a4eb93b3e16be8a4bc99f0c1d3e04417cbb4fcf
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-02 23:56:56 +0100

    objects..

--- components/Landing/Judging/index.tsx
+++ components/Landing/Judging/index.tsx
@@ -13,16 +13,20 @@ const navigationLinks: para[] = [
 		para: "Evaluate the uniqueness and creativity of the solution. Consider whether it addresses a real-world problem in a novel way or introduces innovative features.",
 	},
 	{
-		span: "Overview",
-		para: "#overview",
+		span: "Functionality:",
+		para: "Assess how well the solution works. Does it perform its intended functions effectively and without major issues? Judges would consider the completeness and robustness of the solution.",
 	},
 	{
-		span: "FAQs",
-		para: "#faqs",
+		span: "Impact and Relevance:",
+		para: "Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.",
 	},
     {
-		span: "Contact",
-		para: "#contact",
+		span: "Technical Complexity:",
+		para: "Evaluate the technical sophistication of the solution. Judges would consider the complexity of the code, the use of advanced technologies or algorithms, and the scalability of the solution.",
+	},
+  {
+		span: "Adherence to Hackathon Rules:",
+		para: "Judges will Ensure that the team adhered to the rules and guidelines of the hackathon, including deadlines, use of specific technologies or APIs, and any other competition-specific requirements.",
 	},
 ];
 
