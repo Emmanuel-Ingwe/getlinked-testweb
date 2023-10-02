f11f07c8ddf09799bc2cfa10e80b2d3579cf8b0e
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-02 23:43:15 +0100

    ::

--- components/Landing/Judging/index.tsx
+++ components/Landing/Judging/index.tsx
@@ -9,13 +9,13 @@ const index = (props: Props) => {
     <div className="border-b border-gray-700">
         <section id="" className="container">
             <div className="grid xl:grid-cols-2 gap-5 xl:gap- items-start">
-                <div className="mt-[63px] ">
+                <div className="mt-[193px]">
                   <div className="flex items-center">
                     <img src="/images/8046554-1@2x.png" width={710} height={587} alt="big-idea" />
                   </div>
                 </div>
 
-                <div className="mt-[193px]">
+                <div className="mt-[63px] ">
                   <div className="flex flex-col">
                     <div className="">
                       <h2 className='text-[32px] font-bold'>Judging Criteria</h2>
