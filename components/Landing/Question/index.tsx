d1b75f31310cf0d9767fd0654e936fab7e6abc2d
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-03 05:21:08 +0100

    add: object.question

--- components/Landing/Question/index.tsx
+++ components/Landing/Question/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
-type Props = {}
+type para = {
+  span: string;
+  i: string;
+};
 
-const index = (props: Props) => {
+
+const index = () => {
+  const items: para[] = [
+    {
+      span: "Can I work on a project I started before the hackathon?",
+      i: "+"
+    },
+    {
+      span: "What happens if I need help during the hackathon?",
+      i: "+"
+    },
+    {
+      span: "What happens if I don't have an idea for a project?",
+      i: "+"
+    },
+      {
+      span: "Can I join a team or do I have to come with one?",
+      i: "+"
+    },
+    {
+      span: "What happens after the hackathon ends",
+      i: "+"
+    },
+    {
+      span: "Can I work on a project I started before the hackathon?",
+      i: "+"
+    },
+];
   return (
     <>
     <div className="border-b border-gray-700">
@@ -16,6 +46,12 @@ const index = (props: Props) => {
                   <div className="">
                     <p className="">We got answers to the questions that you might want to ask about getlinked Hackathon 1.0</p>
                   </div>
+                  <div className="mb-[30px] mt-[21px]">
+                        {items.map((item, index) => {
+                            return (
+                          <p className='font-montserrat text-[14px] mb-[26px]'>{item.span} {''} {item.i}</p>
+                        )})}
+                    </div>
                 </div>
 
                 <div className="">
