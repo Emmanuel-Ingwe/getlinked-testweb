9b7c8785f8a8fdb4f0c33375b29abd8ad644e1e7
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-03 05:08:50 +0100

    //

--- components/Landing/Question/index.tsx
+++ components/Landing/Question/index.tsx
@@ -9,17 +9,21 @@ const index = (props: Props) => {
         <section id="" className="container">
             <div className="grid xl:grid-cols-2 gap-5 xl:gap- items-start">
                 <div className="">
-                  <h1 className="">Frequently Asked</h1>
-                  <h1><span className='text-[32px] font-bold text text-fuchsia-600'>Questions</span></h1>
-                </div>
-                <div className="">
-                  <p className="">We got answers to the questions that you might want to ask about getlinked Hackathon 1.0</p>
+                  <div className="">
+                    <h1 className="">Frequently Asked</h1>
+                    <h1><span className='text-[32px] font-bold text text-fuchsia-600'>Questions</span></h1>
+                  </div>
+                  <div className="">
+                    <p className="">We got answers to the questions that you might want to ask about getlinked Hackathon 1.0</p>
+                  </div>
                 </div>
 
                 <div className="">
-                  <div className=""></div>
                   <div className="">
-                    <img src="cwok-casual-21-1@2x.png" alt="casual" />
+                    <img src="/Images/.svg" alt="" />
+                  </div>
+                  <div className="">
+                    <img src="/Images/cwok-casual-21-1@2x.png" alt="casual" />
                   </div>
                 </div>
             </div>
