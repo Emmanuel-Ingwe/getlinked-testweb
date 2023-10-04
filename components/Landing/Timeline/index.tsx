d48bebdc3e44c1319e34ae76d3fd1dd38e202ea8
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-04 07:23:21 +0100

    ..

--- components/Landing/Timeline/index.tsx
+++ components/Landing/Timeline/index.tsx
@@ -7,8 +7,12 @@ const index = (props: Props) => {
     <>
       <section className='container'>
         <div className="flex flex-col items-center mt-[72px]">
-          <h1 className="text-[32px] font-bold">Timeline</h1>
-          <p className="xl:w-[346px] text-center text-[14px]">Here is the breakdown of the time we anticipate using for the upcoming event.</p>
+          <div className="">
+            <h1 className="text-[32px] font-bold text-center">Timeline</h1>
+            <p className="xl:w-[346px] text-center text-[14px] mt-[12px]">Here is the breakdown of the time we anticipate using for the upcoming event.</p>
+          </div>  
+
+          <div className="">ss</div>
         </div>
       </section>
     </>
