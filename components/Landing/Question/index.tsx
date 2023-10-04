1cba1c570c3dfc7647571700ee17b4084bc22267
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-04 06:16:59 +0100

    Question margin

--- components/Landing/Question/index.tsx
+++ components/Landing/Question/index.tsx
@@ -37,22 +37,22 @@ const index = () => {
     <>
     <div className="border-b border-gray-700">
         <section id="" className="container">
-            <div className="grid xl:grid-cols-2 mt-[150px] items-center justify-center">
-                <div className="">
+            <div className="grid xl:grid-cols-2 items-center justify-center">
+                <div className="mt-[100px]">
                   <div className="mb-[16px]">
                     <h1 className="text-[32px] font-bold">Frequently Asked</h1>
                     <h1><span className='text-[32px] font-bold text text-fuchsia-600'>Questions</span></h1>
                   </div>
                   <div className="lead leading-[27px]">
                     <p className="text-[14px]">We got answers to the questions that you might</p>
-                    <p className="text-[14px] mb-[26px]">want to ask about getlinked Hackathon 1.0</p>
+                    <p className="text-[14px] mb-[26px]">want to ask about <span className="font-bold">getlinked Hackathon 1.0</span></p>
                   </div>
                   <div className="mb-[30px] mt-[68px]">
                         {items.map((item, index) => {
                             return (
                           <div className="flex justify-between w-[427px] border pt-[25px] border-b-fuchsia-600">
                             <div className="">
-                              <p className='font-montserrat text-[14px] mb-[26px]'>{item.span}</p>
+                              <p className='font-montserrat text-[14px] mb-[16px]'>{item.span}</p>
                             </div>
                             <div className="j flex justify-end">
                               <span className='text-fuchsia-600 text-[20px] leading-[27px] cursor-pointer'>{item.i}</span>
@@ -62,7 +62,7 @@ const index = () => {
                     </div>
                 </div>
 
-                <div className="">
+                <div className="mt-[150px]">
                   <div className="">
                     <img src="/Images/.svg" alt="" />
                   </div>
