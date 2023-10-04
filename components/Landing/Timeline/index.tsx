5cf370021ac79f1a883728adcf1ae64b1e1958cb
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-04 07:16:12 +0100

    timeline-texts

--- components/Landing/Timeline/index.tsx
+++ components/Landing/Timeline/index.tsx
@@ -4,7 +4,14 @@ type Props = {}
 
 const index = (props: Props) => {
   return (
-    <div>index</div>
+    <>
+      <section className='container'>
+        <div className="flex flex-col items-center mt-[72px]">
+          <h1 className="text-[32px] font-bold">Timeline</h1>
+          <p className="xl:w-[346px] text-center text-[14px]">Here is the breakdown of the time we anticipate using for the upcoming event.</p>
+        </div>
+      </section>
+    </>
   )
 }
 
