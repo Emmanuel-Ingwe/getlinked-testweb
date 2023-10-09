81199fe92c1e316b97e0bac4def92c79ed120c84
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-09 19:31:39 +0100

    :

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,3 +28,35 @@ IndexPage.getLayout = function getLayout(page) {
 	return <MainLayout>{page}</MainLayout>;
 };
 
+
+{/* <div className="pt-48">
+  {items.map((item, index) => {
+    const isEven = index % 2 === 0;
+    const colClass = isEven ? 'lg:flex-row-reverse' : ''; // Apply flex-row-reverse for even items
+
+    return (
+      <div key={index} className={`grid grid-cols-3 ${colClass}`}>
+        <div className="flex flex-col text-right">
+          <h1 className="text-[24px] text-fuchsia-500 font-bold">
+            {item.leftTexth1}
+          </h1>
+          <p className="text-[14px]">
+            {item.leftTextp} <br /> {item.leftspan}
+          </p>
+        </div>
+        <div className="flex justify-center relative">
+          <div className="h-[96px] w-[4px] bg-fuchsia-500 absolute top-0 -mt-[106px]"></div>
+          <div className="rounded-full bg-gradient-general text-white w-10 h-10 flex items-center justify-center text-2xl font-semibold mb-[125px]">
+            {item.number}
+          </div>
+        </div>
+        <div className="flex text-left">
+          <h1 className="text-[24px] text-fuchsia-500 font-bold">
+            {item.rightText}
+          </h1>
+        </div>
+      </div>
+    );
+  })}
+</div>
+ */}
