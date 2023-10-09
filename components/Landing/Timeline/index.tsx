51d09084bf9d1eda057eeb2ad4812c7a68c6a23d
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2023-10-09 14:38:36 +0100

    objects

--- components/Landing/Timeline/index.tsx
+++ components/Landing/Timeline/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 type Item = {
   leftTexth1: string;
   leftTextp: string;
+  leftspan: string;
   number: number;
   rightText: string;
 }
@@ -12,31 +13,43 @@ const index = () => {
     {
       leftTexth1: "Hackathon Announcement",
       leftTextp: "The getlinked tech hackathon 1.0 is formally announced",
+      leftspan: "to the general public and teams begin to get ready to register",
       number: 1,
       rightText: "November 18, 2023",
     },
     {
-      leftTexth1: "Hackathon Announcement",
-      leftTextp: "The getlinked tech hackathon 1.0 is formally announced",
+      leftTexth1: "Teams Registration begins",
+      leftTextp: "Interested teams can now show their interest in the",
+      leftspan: "getlinked tech hackathon 1.0 2023 by proceeding to register",
       number: 2,
       rightText: "November 18, 2023",
     },
     {
-      leftTexth1: "Hackathon Announcement",
-      leftTextp: "The getlinked tech hackathon 1.0 is formally announced",
+      leftTexth1: "Teams Registration ends",
+      leftTextp: "Interested Participants are no longer Allowed to",
+      leftspan: "register",
       number: 3,
       rightText: "November 18, 2023",
     },
     {
-      leftTexth1: "Hackathon Announcement",
-      leftTextp: "The getlinked tech hackathon 1.0 is formally announced",
+      leftTexth1: "Announcement of the accepted teams and ideas",
+      leftTextp: "All teams whom idea has been accepted into getlinked tech",
+      leftspan: "hackathon 1.0 2023 are formally announced",
       number: 4,
       rightText: "November 18, 2023",
     },
     {
-      leftTexth1: "Hackathon Announcement",
-      leftTextp: "The getlinked tech hackathon 1.0 is formally announced",
+      leftTexth1: "Getlinked Hackathon 1.0 Offically Begins",
+      leftTextp: "Accepted teams can now proceed to build their",
+      leftspan: "ground breaking skill driven solutions",
       number: 5,
+      rightText: "November 18, 2023",
+    },
+    {
+      leftTexth1: "Demo Day",
+      leftTextp: "Teams get the opportunity to pitch their projects to judges. The winner",
+      leftspan: "of the hackathon will also be announced on this day",
+      number: 6,
       rightText: "November 18, 2023",
     },
   ];
@@ -54,19 +67,16 @@ const index = () => {
             {items.map((item, index) => {
               return(
                 <div className='grid grid-cols-3'>
-
                     <div className="flex flex-col text-right">
                       <h1 className="text-[24px] text-fuchsia-500 font-bold">{item.leftTexth1}</h1>
-                      <p className="text-[14px]">{item.leftTextp} <br /> to the general public and teams begin to get ready to register</p>
+                      <p className="text-[14px]">{item.leftTextp} <br /> {item.leftspan}</p>
                     </div>
-                    
-                    <div key={index} className="flex justify-center relative w-[50px]">  
+                    <div key={index} className="flex justify-center relative">  
                       <div className="h-[96px] w-[4px] bg-fuchsia-500 absolute top-0 -mt-[106px]"></div>
                       <div className="rounded-full bg-gradient-general text-white w-10 h-10 flex items-center justify-center text-2xl font-semibold mb-[125px]">
                         {item.number}
                       </div>
                     </div>
-
                     <div className="flex text-left">
                       <h1 className="text-[24px] text-fuchsia-500 font-bold">{item.rightText}</h1>
                     </div>
